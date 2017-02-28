5e9f30dd25a26f5863edaaabd5c96e49bc07d377
Max Boguslavsky <maxim@banki.ru>
2017-02-28 13:05:37 +0300

    - finished realization.

--- Search_Sort/quick_sort.py
+++ Search_Sort/quick_sort.py
@@ -4,9 +4,42 @@ Output a sorted list."""
 
 
 def quicksort(array):
+    quickSortHelper(array, 0, len(array) - 1)
 
-    return []
+
+def quickSortHelper(array, first, last):
+    if first < last:
+        splitpoint = partition(array, first, last)
+        quickSortHelper(array, first, splitpoint - 1)
+        quickSortHelper(array, splitpoint + 1, last)
+
+
+def partition(array, first, last):
+    pivotvalue = array[first]
+    leftmark = first + 1
+    rightmark = last
+
+    done = False
+    while not done:
+        while leftmark <= rightmark and array[leftmark] <= pivotvalue:
+            leftmark += 1
+
+        while array[rightmark] >= pivotvalue and rightmark >= leftmark:
+            rightmark -= 1
+
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = array[leftmark]
+            array[leftmark] = array[rightmark]
+            array[rightmark] = temp
+
+    temp = array[first]
+    array[first] = array[rightmark]
+    array[rightmark] = temp
+
+    return rightmark
 
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
-print quicksort(test)
+print test
